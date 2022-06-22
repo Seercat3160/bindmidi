b0823bd6b4795b74f33b11276a4df95afe170d73
Seercat3160 <76834828+Seercat3160@users.noreply.github.com>
2022-06-22 20:32:51 +1000

    Format Code

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,36 @@
 // Code from github.com/seercat3160/midi2key, under the MIT license
 // Adapted from https://github.com/Boddlnagg/midir/blob/e5980808e60570639ff4bb20bc983b08da9193c6/examples/test_read_input.rs, also under the MIT license
 
-use std::io::{stdin, stdout, Write};
 use std::error::Error;
+use std::io::{stdin, stdout, Write};
 
-use midir::{MidiInput, Ignore};
+use midir::{Ignore, MidiInput};
 use midly::{live::LiveEvent, MidiMessage};
-
 
 fn main() {
     match run() {
         Ok(_) => (),
-        Err(err) => println!("Error: {}", err)
+        Err(err) => println!("Error: {}", err),
     }
 }
 
 fn run() -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
-    
+
     let mut midi_in = MidiInput::new("midir reading input")?;
     midi_in.ignore(Ignore::None);
-    
+
     // Get an input port (read from console if multiple are available)
     let in_ports = midi_in.ports();
     let in_port = match in_ports.len() {
         0 => return Err("no input port found".into()),
         1 => {
-            println!("Choosing the only available input port: {}", midi_in.port_name(&in_ports[0]).unwrap());
+            println!(
+                "Choosing the only available input port: {}",
+                midi_in.port_name(&in_ports[0]).unwrap()
+            );
             &in_ports[0]
-        },
+        }
         _ => {
             println!("\nAvailable input ports:");
             for (i, p) in in_ports.iter().enumerate() {
@@ -38,21 +40,30 @@ fn run() -> Result<(), Box<dyn Error>> {
             stdout().flush()?;
             let mut input = String::new();
             stdin().read_line(&mut input)?;
-            in_ports.get(input.trim().parse::<usize>()?)
-                     .ok_or("invalid input port selected")?
+            in_ports
+                .get(input.trim().parse::<usize>()?)
+                .ok_or("invalid input port selected")?
         }
     };
-    
+
     println!("\nOpening connection");
     let in_port_name = midi_in.port_name(in_port)?;
 
     // _conn_in needs to be a named parameter, because it needs to be kept alive until the end of the scope
-    let _conn_in = midi_in.connect(in_port, "midir-read-input", move |stamp, message, _| {
-        println!("{}: {:?} (len = {})", stamp, message, message.len());
-        on_midi(message);
-    }, ())?;
-    
-    println!("Connection open, reading input from '{}' (press enter to exit) ...", in_port_name);
+    let _conn_in = midi_in.connect(
+        in_port,
+        "midir-read-input",
+        move |stamp, message, _| {
+            println!("{}: {:?} (len = {})", stamp, message, message.len());
+            on_midi(message);
+        },
+        (),
+    )?;
+
+    println!(
+        "Connection open, reading input from '{}' (press enter to exit) ...",
+        in_port_name
+    );
 
     input.clear();
     stdin().read_line(&mut input)?; // wait for next enter key press
@@ -66,7 +77,10 @@ fn on_midi(event: &[u8]) {
     match event {
         LiveEvent::Midi { channel, message } => match message {
             MidiMessage::NoteOn { key, vel } => {
-                println!("hit note {} on channel {} with velocity {}", key, channel, vel);
+                println!(
+                    "hit note {} on channel {} with velocity {}",
+                    key, channel, vel
+                );
             }
             _ => {}
         },
