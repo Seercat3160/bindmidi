90a7b49846f989abd801aec11626cf08bd1aacae
Seercat3160 <76834828+Seercat3160@users.noreply.github.com>
2022-12-03 16:38:46 +1100

    Add basic note-binding config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 // Code from github.com/seercat3160/midi2key, under the MIT license
 
+use std::collections::HashMap;
 use std::error::Error;
 use std::io::{stdin, stdout, Write};
 
@@ -8,21 +9,24 @@ use midly::{live::LiveEvent, MidiMessage};
 
 use clap::Parser;
 
-use log::{info, warn};
+use log::{error, info, warn};
 
 fn main() {
     let _args = Args::parse();
 
     simple_logger::SimpleLogger::new().init().unwrap();
 
-    match run() {
+    let config: Midi2keyConfig =
+        penguin_config::Deserializer::file_path("config.json").deserialize();
+
+    match run(config) {
         Ok(_) => (),
         Err(err) => println!("Error: {}", err),
     }
 }
 
 // Setup midir and connect to devices
-fn run() -> Result<(), Box<dyn Error>> {
+fn run(config: Midi2keyConfig) -> Result<(), Box<dyn Error>> {
     let mut input = String::new();
 
     let mut midi_in = MidiInput::new("midir reading input")?;
@@ -72,12 +76,42 @@ fn run() -> Result<(), Box<dyn Error>> {
                             let key = key.as_int();
                             let vel = vel.as_int();
                             info!("hit note {} with vel {}", key, vel);
+
+                            // Check if key bound in config, and if so execute any bindings
+                            if config.bindings.contains_key(&key) {
+                                match config.bindings.get(&key) {
+                                    Some(i) => {
+                                        for binding in i {
+                                            let binding_name = binding.0;
+                                            let binding_args = binding.1;
+
+                                            invoke_binding(binding_name, binding_args, true, &vel, &key)
+                                        }
+                                    },
+                                    None => error!("weird state that shouldn't be possible has been reached - type 1")
+                                }
+                            }
                         };
                     }
                     MidiMessage::NoteOff { key, vel: _ } => {
                         {
                             let key = key.as_int();
                             info!("released note {}", key);
+
+                            // Check if key bound in config, and if so execute any bindings
+                            if config.bindings.contains_key(&key) {
+                                match config.bindings.get(&key) {
+                                    Some(i) => {
+                                        for binding in i {
+                                            let binding_name = binding.0;
+                                            let binding_args = binding.1;
+
+                                            invoke_binding(binding_name, binding_args, false, &0, &key)
+                                        }
+                                    },
+                                    None => error!("weird state that shouldn't be possible has been reached - type 2")
+                                }
+                            }
                         };
                     }
                     _ => {}
@@ -99,15 +133,50 @@ fn run() -> Result<(), Box<dyn Error>> {
     warn!("Closing connection");
     Ok(())
 }
+
+fn invoke_binding(binding: &str, args: &Vec<String>, state: bool, vel: &u8, key: &u8) {
+    // Binding is the string for the binding (verbatim from the config)
+    // Args is the binding's args from the config
+    // State = true -> note hit
+    // State = false -> note release
+    // Vel is the note's velocity, or 0 for a release
+
+    match state {
+        true => match binding {
+            "trace" => {
+                warn!("Trace binding hit for key {} during note hit!", key)
+            }
+            _ => {
+                error!(
+                    "Config contains non-implemented binding {} in key {}",
+                    binding, key
+                );
+            }
+        },
+        false => match binding {
+            "trace" => {
+                warn!("Trace binding hit for key {} during note release!", key)
+            }
+            _ => {
+                error!(
+                    "Config contains non-implemented binding {} in key {}",
+                    binding, key
+                );
+            }
+        },
+    }
+}
 // Parse arguments
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
-    /// Config file location
-    #[clap(short, long, value_parser)]
-    config: String,
-
     /// Verbose mode
     #[clap(short, long, value_parser, default_value_t = false)]
     verbose: bool,
+}
+
+// Config file
+#[derive(penguin_config::Deserialize)]
+struct Midi2keyConfig {
+    bindings: HashMap<u8, HashMap<String, Vec<String>>>,
 }
